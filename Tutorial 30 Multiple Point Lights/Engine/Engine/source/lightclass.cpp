ac951ce0bca3e14bea5cf94f929cf63c6b379065
sportsradical <peereira@live.com.pt>
2019-07-15 16:32:13 +0100

    Add files via upload

---
+++ Tutorial 30 Multiple Point Lights/Engine/Engine/source/lightclass.cpp
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: lightclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "lightclass.h"
+
+
+LightClass::LightClass()
+{
+}
+
+
+LightClass::LightClass(const LightClass& other)
+{
+}
+
+
+LightClass::~LightClass()
+{
+}
+
+
+void LightClass::SetDiffuseColor(float red, float green, float blue, float alpha)
+{
+	m_diffuseColor = XMFLOAT4(red, green, blue, alpha);
+}
+
+
+void LightClass::SetPosition(float x, float y, float z)
+{
+	m_position = XMFLOAT4(x, y, z, 1.0f);
+}
+
+
+XMFLOAT4 LightClass::GetDiffuseColor()
+{
+	return m_diffuseColor;
+}
+
+
+XMFLOAT4 LightClass::GetPosition()
+{
+	return m_position;
+}
